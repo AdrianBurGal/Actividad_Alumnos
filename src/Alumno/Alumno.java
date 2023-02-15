41318a7b838a0a3c24db49407fdb3f7f83445091
Adrián Burgos Gálvez <adrianburgosgalvez@gmail.com>
2023-02-15 20:07:43 +0100

    quinto cambio: documentamos el código con javadoc

--- src/Alumno/Alumno.java
+++ src/Alumno/Alumno.java
@@ -1,4 +1,3 @@
-
 package Alumno;
 
 /**
@@ -11,18 +10,38 @@ public class Alumno {
     private String nombre;
     private double nota;
 
+    /**
+     * Devuelve el nombre del alumno.
+     *
+     * @return Devuelve el nombre de ese alumno
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Recibe el nombre del alumno.
+     *
+     * @param nombre Nombre del alumno en cuestión
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Devuelve la nota del alumno.
+     *
+     * @return Devuelve la nota de dicho alumno
+     */
     public double getNota() {
         return nota;
     }
 
+    /**
+     * Recibe la nota del alumno.
+     *
+     * @param nota Nota del alumno
+     */
     public void setNota(double nota) {
         this.nota = nota;
     }
--- src/alumnos/Alumnos.java
+++ src/alumnos/Alumnos.java
@@ -6,30 +6,20 @@ import java.util.Scanner;
 
 public class Alumnos {
 
-    public static double mostrarDatos(Alumno[] alumnos, double sumaDeMedias) {
+    /**
+     * Pide los datos (nombre de los alumnos y nota de los alumnos).
+     *
+     * @param alumnos Nombre de los alumnos
+     */
+    public static void pedirDatos(Alumno[] alumnos) {
+
+        Scanner entrada = new Scanner(System.in);
 
         for (int numAlumno = 0; numAlumno < alumnos.length; numAlumno++) {
-            
-            Alumno alumno = alumnos[numAlumno];
 
-            System.out.println("Alumno " + (numAlumno + 1));
-            System.out.println("Nombre: " + alumno.getNombre());
-            System.out.println("Nota media: " + alumno.getNota());
-            System.out.println("----------------------------");
-
-            sumaDeMedias += alumno.getNota();
-        }
-
-        return sumaDeMedias;
-    }
-
-    public static void pedirDatos(Alumno[] alumnos, Scanner entrada) {
-
-        for (int numAlumno = 0; numAlumno < alumnos.length; numAlumno++) {
-            
             Alumno alumno = new Alumno();
             double notaIntroducida;
-            
+
             System.out.println("Alumno " + (numAlumno + 1));
             System.out.print("Nombre: ");
             String nombreIntroducido = entrada.nextLine();
@@ -42,20 +32,44 @@ public class Alumnos {
         }
     }
 
+    /**
+     * Muestra los datos que se les paso en el anterior método y devuelve la
+     * media de las notas.
+     *
+     * @param alumnos Nombre del alumno
+     * @param sumaNotas Suma las notas de los alumnos para después hacer la
+     * media
+     * @return Devuelve la suma de las notas de los alumnos
+     */
+    public static double mostrarDatos(Alumno[] alumnos, double sumaNotas) {
+
+        for (int numAlumno = 0; numAlumno < alumnos.length; numAlumno++) {
+
+            Alumno alumno = alumnos[numAlumno];
+
+            System.out.println("Alumno " + (numAlumno + 1));
+            System.out.println("Nombre: " + alumno.getNombre());
+            System.out.println("Nota media: " + alumno.getNota());
+            System.out.println("----------------------------");
+
+            sumaNotas += alumno.getNota();
+        }
+
+        return sumaNotas;
+    }
+
     public static void main(String[] args) {
 
         Alumno[] alumnos = new Alumno[5];
-        double sumaDeMedias = 0;
-
-        Scanner entrada = new Scanner(System.in);
+        double sumaNotas = 0;
 
         System.out.println("A continuación deberá introducir el nombre y la nota de los 5 alumnos.");
-        pedirDatos(alumnos, entrada);
+        pedirDatos(alumnos);
 
         System.out.println("Los datos introducidos son los siguientes:");
-        sumaDeMedias = mostrarDatos(alumnos, sumaDeMedias);
+        sumaNotas = mostrarDatos(alumnos, sumaNotas);
 
-        System.out.println("La media global de la clase es " + (sumaDeMedias / alumnos.length));
+        System.out.println("La media global de la clase es " + (sumaNotas / alumnos.length));
     }
 
 }
